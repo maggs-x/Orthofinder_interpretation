fa44476124c2187f3c32b4aafa5c883b36b32873
maggs-x <131017774+maggs-x@users.noreply.github.com>
2024-10-07 22:56:02 +1100

    Update orthologs_assignsymbols.sh

--- orthologs_assignsymbols.sh
+++ orthologs_assignsymbols.sh
@@ -61,3 +61,6 @@ done < Astyanax_mexicanus_GCA_023375975_1_2022_07_pep__v__Homo_sapiens.GRCh38.pe
 #2) append_Hsapsymbols_cleanup.sh
 
 #if you want to run it on a specific list of genes instead of the entire orthology file use append_line_fullorthology.sh
+#1) orthologs_assignsymbols.sh
+#2) append_line_fullorthology.sh
+#3) append_Hsapsymbols_cleanup.sh (this creates an additional field that just has the human gene symbols)
